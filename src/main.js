ae069a6d22cbdb14069dce91a0faf8604d07e5e6
unknown <silence7637>
2019-08-09 13:29:11 +0800

    购看

---
+++ src/main.js
@@ -0,0 +1,166 @@
+// 入口文件
+import Vue from 'vue'
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 注册 vuex
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+// 每次刚进入 网站，肯定会 调用 main.js 在刚调用的时候，先从本地存储中，把 购物车的数据读出来，放到 store 中
+var car = JSON.parse(localStorage.getItem('car') || '[]')
+
+var store = new Vuex.Store({
+  state: { // this.$store.state.***
+    car: car // 将 购物车中的商品的数据，用一个数组存储起来，在 car 数组中，存储一些商品的对象， 咱们可以暂时将这个商品对象，设计成这个样子
+    // { id:商品的id, count: 要购买的数量, price: 商品的单价，selected: false  }
+  },
+  mutations: { // this.$store.commit('方法的名称', '按需传递唯一的参数')
+    addToCar(state, goodsinfo) {
+
+      var flag = false
+
+      state.car.some(item => {
+        if (item.id == goodsinfo.id) {
+          item.count += parseInt(goodsinfo.count)
+          flag = true
+          return true
+        }
+      })
+
+
+      if (!flag) {
+        state.car.push(goodsinfo)
+      }
+
+
+      localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    updateGoodsInfo(state, goodsinfo) {
+      // 修改购物车中商品的数量值
+      // 分析：
+      state.car.some(item => {
+        if (item.id == goodsinfo.id) {
+          item.count = parseInt(goodsinfo.count)
+          return true
+        }
+      })
+      // 当修改完商品的数量，把最新的购物车数据，保存到 本地存储中
+      localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    removeFormCar(state, id) {
+
+      state.car.some((item, i) => {
+        if (item.id == id) {
+          state.car.splice(i, 1)
+          return true;
+        }
+      })
+      // 将删除完毕后的，最新的购物车数据，同步到 本地存储中
+      localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    updateGoodsSelected(state, info) {
+      state.car.some(item => {
+        if (item.id == info.id) {
+          item.selected = info.selected
+        }
+      })
+      // 把最新的 所有购物车商品的状态保存到 store 中去
+      localStorage.setItem('car', JSON.stringify(state.car))
+    }
+  },
+  getters: { // this.$store.getters.***
+    // 相当于 计算属性，也相当于 filters
+    getAllCount(state) {
+      var c = 0;
+      state.car.forEach(item => {
+        c += item.count
+      })
+      return c
+    },
+    getGoodsCount(state) {
+      var o = {}
+      state.car.forEach(item => {
+        o[item.id] = item.count
+      })
+      return o
+    },
+    getGoodsSelected(state) {
+      var o = {}
+      state.car.forEach(item => {
+        o[item.id] = item.selected
+      })
+      return o
+    },
+    getGoodsCountAndAmount(state) {
+      var o = {
+        count: 0, // 勾选的数量
+        amount: 0 // 勾选的总价
+      }
+      state.car.forEach(item => {
+        if (item.selected) {
+          o.count += item.count
+          o.amount += item.price * item.count
+        }
+      })
+      return o
+    }
+  }
+})
+
+//导入时间插件
+import moment from 'moment'
+
+//定义全局过滤器
+Vue.filter('dateFormant',function (dataStr,pattern = "YYYY-MM-DD HH-mm-ss") {
+  return moment(dataStr).format(pattern)
+})
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+
+// Vue.http.option.root = 'http://www.liulongbin.top:3005'
+Vue.http.options.emulateJSON = true
+
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
+
+// 按需导入 Mint-UI 中的组件
+// import { Header, Swipe, SwipeItem,Button,Lazyload } from 'mint-ui'
+// Vue.component(Header.name, Header)
+// Vue.component(Swipe.name, Swipe)
+// Vue.component(SwipeItem.name, SwipeItem)
+// Vue.component(Button.name, Button)
+// Vue.use(Lazyload)
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
+import 'mint-ui/lib/style.css'
+
+// 安装 图片预览插件
+import VuePreview from 'vue-preview'
+Vue.use(VuePreview)
+
+
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
+
+
+
+// 导入 App 根组件
+import app from './App.vue'
+
+var vm = new Vue({
+  el: '#app',
+  render: c => c(app),
+  router, // 1.4 挂载路由对象到 VM 实例上
+  store
+})
